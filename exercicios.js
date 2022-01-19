bb962fab511538b8f0c14432468e25c76ca8c314
FabioCrippa <crippa.fabiocrippa@gmail.com>
2022-01-19 19:42:02 -0300

    primeiro commit

---
+++ exercicios.js
@@ -0,0 +1,16 @@
+//Object date=>
+//pega a data atual, mes, ano...e imprime
+
+//Estrutura
+//const nomeVariavel = new Date();
+//console.log(nomeVariavel.getDay()) => imprime a posicao do dia da semana
+           //(nomeVariavel.getMonth()) => imprime a posicao do mes
+           //(nomeVariavel.getMonth() + 1) => imprime o mes exato
+           //(nomeVariavel.getFullYear()) => imprime o ano exato
+           //(nomeVariavel.getDate()) => imprime o dia
+const data = new Date()
+console.log(data.getDay())
+console.log(data.toString())
+//...
+
+
